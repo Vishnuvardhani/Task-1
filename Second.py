c2898fdc6874c73889e84af2031d358135f5cb9f
Vishnu <vishnuvpinnapareddy@gmail.com>
2024-06-29 23:15:17 -0500

    added new python code

---
+++ Second.py
@@ -0,0 +1,16 @@
+print("Program for Calculator")
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+print("Sum of a and b is", a+b)
+print("Sub of a and b is", a-b)
+print("Multiplication of a and b is", a*b)
+print("Division of a and b is", a/b)
+x = a*a
+print("Square value of a is", x)
+y=b*b*b
+print("Cube value of b is", y)
+if(x>y):
+    print("x is greater than y")
+else:
+    print("y is greater than x")
+
